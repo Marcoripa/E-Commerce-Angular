74da6b2674f1a9eab2dec6f32e258a77bd51000a
Marco Ripa <marcoripa@hotmail.it>
2022-08-26 15:45:47 +0200

    Improve search bars

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -8,24 +8,50 @@ import { HttpService } from 'src/app/services/http.service';
 })
 export class HomeComponent implements OnInit {
   items: any;
-  category: string = '';
-  selected: string = '';
+  renderedItems: any;
+  category = '';
+  selected = '';
+  input = '';
+  price = '';
 
   constructor(private service: HttpService) {}
 
+  private getItems(category: string | number) {
+    this.service.getItems(`${category}`).subscribe((res) => {
+      this.items = res; 
+      this.renderedItems = res;   
+    });
+  }
+
+  //Call the api at the first render of the component
   ngOnInit(): void {
     this.getItems(this.category);
   }
 
   //Call the api with the value taken from the select option
   onChange(event: string) {
-    this.getItems(event)
+    this.getItems(event);
   }
 
-  private getItems(category: string) {
-    this.service.getItems(`${category}`).subscribe((res) => {
-      this.items = res;
-      console.log(res);
-    });
+  inputChange(event: string) {
+    this.input = event;
+  }
+
+  submitInput() {
+    const filteredByName = this.items.filter((element: any) =>
+      element.title.includes(this.input)
+    );
+    if (filteredByName !== undefined) {
+      this.renderedItems = filteredByName;
+    }
+  }
+
+  priceChange(event: any) {
+    const filteredByPrice = this.items.filter(
+      (element: any) => element.price < event
+    );
+    if (filteredByPrice !== undefined) {
+       this.renderedItems = filteredByPrice;
+    }
   }
 }
